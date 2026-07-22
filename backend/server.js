6ee7fbee658887511a89efbb10a1f43f2f938142
mohsinshhzad88-sudo <mohsinshhzad88@gmail.com>
2026-07-22 14:57:00 +0500

    Day 2: Connect React frontend with Express backend

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,21 @@ const app = express();
 
 app.use(cors());
 
+// Home route
 app.get("/", (req, res) => {
   res.send("🚀 Welcome to Document Analyzer Backend!");
 });
 
+// API route
+app.get("/api/message", (req, res) => {
+  res.json({
+    message: "Hello from Express! 🚀"
+  });
+});
+
 const PORT = 5000;
 
+// Start server
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
