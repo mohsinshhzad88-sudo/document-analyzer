7cadf5b700ddf1dbe48b5e2dfd15c8ee36c0fc22
mohsinshhzad88-sudo <mohsinshhzad88@gmail.com>
2026-07-22 17:12:34 +0500

    Day 4: Add PDF text extraction

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const { PDFParse } = require("pdf-parse");
+const fs = require("fs");
 const cors = require("cors");
 const multer = require("multer");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+
+// File storage setup
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -14,33 +21,79 @@ const storage = multer.diskStorage({
   },
 });
 
+
 const upload = multer({ storage });
 
-app.use(cors());
 
 // Home route
 app.get("/", (req, res) => {
   res.send("🚀 Welcome to Document Analyzer Backend!");
 });
 
-// API route
+
+// Test API route
 app.get("/api/message", (req, res) => {
   res.json({
     message: "Hello from Express! 🚀"
   });
 });
 
-const PORT = 5000;
 
+// Upload document
 app.post("/api/upload", upload.single("document"), (req, res) => {
+
   res.json({
     success: true,
     message: "File uploaded successfully!",
-    file: req.file,
+    file: req.file.filename,
+    path: req.file.path
   });
+
 });
 
+
+// PDF text extraction test
+app.get("/api/test-pdf", async (req, res) => {
+
+  try {
+
+    const dataBuffer = fs.readFileSync(
+      "uploadss/"+req.query.file
+    );
+
+
+    const parser = new PDFParse({
+      data: dataBuffer
+    });
+
+
+    const result = await parser.getText();
+
+
+    res.json({
+      text: result.text
+    });
+
+
+  } catch (error) {
+
+    console.log("PDF ERROR:", error);
+
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+});
+
+
 // Start server
+const PORT = 5000;
+
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(
+    `✅ Server running on http://localhost:${PORT}`
+  );
 });
